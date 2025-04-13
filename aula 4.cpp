0188bdbb307d411a6803b217896081d3b4df7f67
KauaRios <kauarios49@gmail.com>
2025-04-13 17:01:06 -0300

    Add files via upload

---
+++ aula 4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+//tipo nome
+//tipo nome =valor
+
+int vidas; = //25,10,16
+char letra = //"b";
+double decimal; //2,5
+float decimaa1; //2,49999999
+float decimal 2; //2,5
+bool vivo; //true=verdadeiro / false=falso
+string nome; // "lucas"
+    return 0;
+}
