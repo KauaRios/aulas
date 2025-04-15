e60ddaa1d4900a99b26dd099571bd40fef43ca91
KauaRios <kauarios49@gmail.com>
2025-04-15 09:31:50 -0300

    Update tabuada.cpp

--- tabuada.cpp
+++ tabuada.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string>
+#include <iomanip> 
 
 using namespace std;
 
@@ -8,18 +8,29 @@ int main() {
 
     cout<<"digite um numero : "<<endl;
     cin>>valor;
-
+    cout<<setw(10)<<endl;
     cout<<valor<<"x"<<0<<"="<<valor*0<<endl;
+    cout<<setw(10)<<endl;
     cout<<valor<<"x"<<1<<"="<<valor*1<<endl; 
+    cout<<setw(10)<<endl;
     cout<<valor<<"x"<<2<<"="<<valor*2<<endl;
+    cout<<setw(10)<<endl;
     cout<<valor<<"x"<<3<<"="<<valor*3<<endl;
+    cout<<setw(10)<<endl;
     cout<<valor<<"x"<<4<<"="<<valor*4<<endl;
+    cout<<setw(10)<<endl;
     cout<<valor<<"x"<<5<<"="<<valor*5<<endl;
+    cout<<setw(10)<<endl;
     cout<<valor<<"x"<<6<<"="<<valor*6<<endl;
+    cout<<setw(10)<<endl;
     cout<<valor<<"x"<<7<<"="<<valor*7<<endl;
+    cout<<setw(10)<<endl;
     cout<<valor<<"x"<<8<<"="<<valor*8<<endl;
+    cout<<setw(10)<<endl;
     cout<<valor<<"x"<<9<<"="<<valor*9<<endl;
-    cout<<valor<<"x"<<10<<"="<<valor*10<<endl;       
+    cout<<setw(10)<<endl;
+    cout<<valor<<"x"<<10<<"="<<valor*10<<endl;  
+    cout<<setw(10)<<endl;     
 
 
 
