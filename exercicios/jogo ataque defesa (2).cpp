132fbab40be9cd7ec0b6dad903971f110837f935
Kaua Rios <kauarios49@gmail.com>
2025-05-24 09:54:42 -0300

    Add files via upload

---
+++ exercicios/jogo ataque defesa (2).cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int jogarNovamente = 1;
+
+    while (jogarNovamente == 1) {
+
+        string nome, defnome;
+        int ataque=0, poder=0, escudo=0;
+        float vida = 0;
+        int continuar = 1;
+
+        cout << "Bem-vindo ao jogo de ataque e defesa!" << endl;
+
+        cout << "Digite o nome do personagem: "<<endl;
+        cin >> nome;
+
+        cout << "Insira o nome do defensor: "<<endl;
+        cin >> defnome;
+
+        cout << "Insira o poder de ataque: "<<endl;
+        cin >> ataque;
+
+        cout << "Insira a vida do defensor: "<<endl;
+        cin >> vida;
+
+        cout << "Insira o poder de defesa: "<<endl;
+        cin >> poder;
+
+        do {
+            cout << "O personagem tem escudo? Digite apenas 1 para sim ou 0 para nao: "<<endl;
+            cin >> escudo;
+
+            while (escudo != 1 && escudo != 0) {
+                cout << "Valor inválido! Digite apenas 1 para sim ou 0 para nao: "<<endl;
+                cin >> escudo;
+            }
+
+            if (escudo == 1) {
+                cout << "O personagem tem escudo!" << endl;
+            } else {
+                cout << "O personagem não tem escudo!" << endl;
+            }
+
+            cout << "A vida do defensor " << defnome << " agora é: " << vida << endl;
+
+            int dano = 0;
+
+            if (ataque > poder && escudo == 1) {
+                dano = ataque - poder;
+                cout << "O personagem " << nome << " atacou " << defnome << " e causou " << dano << " de dano" << endl;
+                vida -= dano;
+            } else if (ataque > poder && escudo == 0) {
+                dano = (ataque - poder) / 2;
+                cout << "O personagem " << nome << " atacou " << defnome << " e causou " << dano << " de dano" << endl;
+                vida -= dano;
+            } else {
+                cout << "O personagem " << nome << " atacou " << defnome << " e causou 0 de dano" << endl;
+            }
+
+            cout << "A vida do defensor " << defnome << " agora é: " << vida << endl;
+
+            if (vida <= 0) {
+                cout << "O defensor " << defnome << " foi derrotado!" << endl;
+                break;
+            }
+
+            cout << "Deseja continuar atacando? 1 para sim e 0 para nao: "<<endl;
+            cin >> continuar;
+
+        } while (continuar == 1 && vida > 0);
+
+        cout << "Fim do jogo!" << endl;
+
+        cout << "Deseja jogar novamente? 1 para sim e 0 para nao: "<< endl;
+        cin >> jogarNovamente;
+    }
+
+    cout << "Obrigado por jogar!" << endl;
+
+    return 0;
+}
