19d2c706a1d1134d174d8e7ada0a931ca3718b6d
KauaRios <kauarios49@gmail.com>
2025-10-21 18:11:38 -0300

    adicionado pasta aulasc++

---
+++ aulasc++/multiplicaçao com for.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+float valor1=0;float multi=1;
+for(int i=1; i<5; i++){
+
+  cout<<"digite o valor "<< i <<": ";
+cin>>valor1;
+   
+
+ multi*= valor1;
+}
+
+    cout<< "o valor da multiplicação é: "<< multi << endl;
+
+
+
+
+
+
+return 0;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
