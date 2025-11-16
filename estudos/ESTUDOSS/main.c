dfc59ea01ae8c01b1f3f6b7b2c4c21c8456aba25
KauaRios <kauarios49@gmail.com>
2025-11-16 11:46:38 -0300

    ADD

---
+++ estudos/ESTUDOSS/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //como criar um Vetor
+    int idade;
+    char sexo;
+    float n1;
+
+    int num1[10];
+    int num2[]={1,5,9,15,43};
+    int num3[5]={4,7,5};  //0,1,2
+    int num4[5]{1,2,3,4,5,6};
+
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
---
+++ estudos/ESTUDOSS/main2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+int main(){
+
+//como criar um vetor
+int idade;
+int sexo;
+float n1;
+
+int num1[10];
+int num2[]={1,5,9,15,43};
+int num3[5]={4,7,5};
+int num4[5]={1,2,3,4,5,6};
+
+return 0;
+}
+
+
+
